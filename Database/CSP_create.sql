449cf373a073db52a136e47c87f438f5c3710e91
Shona Lowden <lowdensh@gmail.com>
2020-08-03 21:25:15 +0100

    update database schema

--- Database/CSP_create.sql
+++ Database/CSP_create.sql
@@ -20,46 +20,22 @@ GO
 ALTER TABLE [dbo].[CSP_Response] DROP CONSTRAINT [FK_CSP_Response_CSP_Question]
 GO
 
-ALTER TABLE [dbo].[CSP_Response] DROP CONSTRAINT [FK_CSP_Response_CSP_V_Category_itrs_good]
+ALTER TABLE [dbo].[CSP_V_Analysis] DROP CONSTRAINT [FK_CSP_V_Analysis_CSP_V_Category]
 GO
 
-ALTER TABLE [dbo].[CSP_Response] DROP CONSTRAINT [FK_CSP_Response_CSP_V_Category_itrs_passive]
+ALTER TABLE [dbo].[CSP_V_Analysis] DROP CONSTRAINT [FK_CSP_V_Analysis_CSP_V_Area]
 GO
 
-ALTER TABLE [dbo].[CSP_Response] DROP CONSTRAINT [FK_CSP_Response_CSP_V_Category_itrs_poor]
-GO
-
-ALTER TABLE [dbo].[CSP_Response] DROP CONSTRAINT [FK_CSP_Response_CSP_V_Category_product_good]
-GO
-
-ALTER TABLE [dbo].[CSP_Response] DROP CONSTRAINT [FK_CSP_Response_CSP_V_Category_product_passive]
-GO
-
-ALTER TABLE [dbo].[CSP_Response] DROP CONSTRAINT [FK_CSP_Response_CSP_V_Category_product_poor]
-GO
-
-ALTER TABLE [dbo].[CSP_Response] DROP CONSTRAINT [FK_CSP_Response_CSP_V_Category_support_good]
-GO
-
-ALTER TABLE [dbo].[CSP_Response] DROP CONSTRAINT [FK_CSP_Response_CSP_V_Category_support_passive]
-GO
-
-ALTER TABLE [dbo].[CSP_Response] DROP CONSTRAINT [FK_CSP_Response_CSP_V_Category_support_poor]
-GO
-
-ALTER TABLE [dbo].[CSP_Response] DROP CONSTRAINT [FK_CSP_Response_CSP_V_Category_docs_good]
-GO
-
-ALTER TABLE [dbo].[CSP_Response] DROP CONSTRAINT [FK_CSP_Response_CSP_V_Category_docs_passive]
-GO
-
-ALTER TABLE [dbo].[CSP_Response] DROP CONSTRAINT [FK_CSP_Response_CSP_V_Category_docs_poor]
+ALTER TABLE [dbo].[CSP_V_Analysis] DROP CONSTRAINT [FK_CSP_V_Analysis_CSP_Response]
 GO
 
 
 /********************
 	DROP TABLES
 *********************/
+DROP TABLE [dbo].[CSP_Product$]
+GO
+
 DROP TABLE [dbo].[CSP_Survey]
 GO
 
@@ -72,7 +48,13 @@ GO
 DROP TABLE [dbo].[CSP_Response]
 GO
 
+DROP TABLE [dbo].[CSP_V_Area]
+GO
+
 DROP TABLE [dbo].[CSP_V_Category]
+GO
+
+DROP TABLE [dbo].[CSP_V_Analysis]
 GO
 
 
@@ -85,6 +67,14 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+CREATE TABLE [dbo].[CSP_Product$](
+	[Company] [nvarchar](255) NULL,
+	[Domain] [nvarchar](255) NULL,
+	[Product] [nvarchar](255) NULL,
+	[Region] [nvarchar](255) NULL
+) ON [PRIMARY]
+GO
+
 CREATE TABLE [dbo].[CSP_Survey](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[name] [nvarchar](50) NOT NULL,
@@ -95,7 +85,6 @@ CREATE TABLE [dbo].[CSP_Survey](
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
-
 GO
 
 CREATE TABLE [dbo].[CSP_Survey_User](
@@ -104,34 +93,22 @@ CREATE TABLE [dbo].[CSP_Survey_User](
 	[user_id] [bigint] NULL,
 	[email] [nvarchar](200) NOT NULL,
 	[end_date] [datetime] NOT NULL,
+	[deleted] [bit] NULL,
  CONSTRAINT [PK_CSP_Survey_User] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
-
 GO
 
 CREATE TABLE [dbo].[CSP_Question](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[question] [nvarchar](max) NOT NULL,
  CONSTRAINT [PK_CSP_Question] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
-GO
-
-CREATE TABLE [dbo].[CSP_V_Category](
-	[id] [int] NOT NULL,
-	[category] [nvarchar](50) NOT NULL,
- CONSTRAINT [PK_CSP_V_Category] PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
 GO
 
 CREATE TABLE [dbo].[CSP_Response](
@@ -140,24 +117,45 @@ CREATE TABLE [dbo].[CSP_Response](
 	[question_id] [int] NOT NULL,
 	[score] [tinyint] NULL,
 	[comment] [nvarchar](max) NULL,
-	[v_itrs_good] [int] NULL,
-	[v_itrs_passive] [int] NULL,
-	[v_itrs_poor] [int] NULL,
-	[v_product_good] [int] NULL,
-	[v_product_passive] [int] NULL,
-	[v_product_poor] [int] NULL,
-	[v_support_good] [int] NULL,
-	[v_support_passive] [int] NULL,
-	[v_support_poor] [int] NULL,
-	[v_docs_good] [int] NULL,
-	[v_docs_passive] [int] NULL,
-	[v_docs_poor] [int] NULL,
+	[deleted] [bit] NULL,
  CONSTRAINT [PK_CSP_Response] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
 
+CREATE TABLE [dbo].[CSP_V_Area](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[area] [nvarchar](20) NOT NULL,
+	[opinion] [nvarchar](20) NOT NULL,
+ CONSTRAINT [PK_CSP_V_Area] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[CSP_V_Category](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[category] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_CSP_V_Category] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[CSP_V_Analysis](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[response_id] [int] NOT NULL,
+	[area_id] [int] NOT NULL,
+	[category_id] [int] NOT NULL,
+ CONSTRAINT [PK_CSP_V_Analysis] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
 
@@ -170,137 +168,109 @@ GO
 ALTER TABLE [dbo].[CSP_Survey_User]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Survey_User_CSP_Survey] FOREIGN KEY([survey_id])
 REFERENCES [dbo].[CSP_Survey] ([id])
 GO
+
 ALTER TABLE [dbo].[CSP_Survey_User] CHECK CONSTRAINT [FK_CSP_Survey_User_CSP_Survey]
 GO
 
 ALTER TABLE [dbo].[CSP_Survey_User]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Survey_User_Users] FOREIGN KEY([user_id])
 REFERENCES [dbo].[Users] ([id])
 GO
+
 ALTER TABLE [dbo].[CSP_Survey_User] CHECK CONSTRAINT [FK_CSP_Survey_User_Users]
 GO
 
 ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_Question] FOREIGN KEY([question_id])
 REFERENCES [dbo].[CSP_Question] ([id])
 GO
+
 ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_Question]
 GO
 
 ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_Survey_User] FOREIGN KEY([survey_user_id])
 REFERENCES [dbo].[CSP_Survey_User] ([id])
 GO
+
 ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_Survey_User]
 GO
 
-ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_V_Category_itrs_good] FOREIGN KEY([v_itrs_good])
-REFERENCES [dbo].[CSP_V_Category] ([id])
-GO
-ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_V_Category_itrs_good]
+ALTER TABLE [dbo].[CSP_V_Analysis]  WITH CHECK ADD  CONSTRAINT [FK_CSP_V_Analysis_CSP_Response] FOREIGN KEY([response_id])
+REFERENCES [dbo].[CSP_Response] ([id])
 GO
 
-ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_V_Category_itrs_passive] FOREIGN KEY([v_itrs_passive])
-REFERENCES [dbo].[CSP_V_Category] ([id])
-GO
-ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_V_Category_itrs_passive]
+ALTER TABLE [dbo].[CSP_V_Analysis] CHECK CONSTRAINT [FK_CSP_V_Analysis_CSP_Response]
 GO
 
-ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_V_Category_itrs_poor] FOREIGN KEY([v_itrs_poor])
-REFERENCES [dbo].[CSP_V_Category] ([id])
-GO
-ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_V_Category_itrs_poor]
+ALTER TABLE [dbo].[CSP_V_Analysis]  WITH CHECK ADD  CONSTRAINT [FK_CSP_V_Analysis_CSP_V_Area] FOREIGN KEY([area_id])
+REFERENCES [dbo].[CSP_V_Area] ([id])
 GO
 
-ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_V_Category_product_good] FOREIGN KEY([v_product_good])
-REFERENCES [dbo].[CSP_V_Category] ([id])
-GO
-ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_V_Category_product_good]
+ALTER TABLE [dbo].[CSP_V_Analysis] CHECK CONSTRAINT [FK_CSP_V_Analysis_CSP_V_Area]
 GO
 
-ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_V_Category_product_passive] FOREIGN KEY([v_product_passive])
+ALTER TABLE [dbo].[CSP_V_Analysis]  WITH CHECK ADD  CONSTRAINT [FK_CSP_V_Analysis_CSP_V_Category] FOREIGN KEY([category_id])
 REFERENCES [dbo].[CSP_V_Category] ([id])
-GO
-ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_V_Category_product_passive]
 GO
 
-ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_V_Category_product_poor] FOREIGN KEY([v_product_poor])
-REFERENCES [dbo].[CSP_V_Category] ([id])
-GO
-ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_V_Category_product_poor]
-GO
-
-ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_V_Category_support_good] FOREIGN KEY([v_support_good])
-REFERENCES [dbo].[CSP_V_Category] ([id])
-GO
-ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_V_Category_support_good]
-GO
-
-ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_V_Category_support_passive] FOREIGN KEY([v_support_passive])
-REFERENCES [dbo].[CSP_V_Category] ([id])
-GO
-ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_V_Category_support_passive]
-GO
-
-ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_V_Category_support_poor] FOREIGN KEY([v_support_poor])
-REFERENCES [dbo].[CSP_V_Category] ([id])
-GO
-ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_V_Category_support_poor]
-GO
-
-ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_V_Category_docs_good] FOREIGN KEY([v_docs_good])
-REFERENCES [dbo].[CSP_V_Category] ([id])
-GO
-ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_V_Category_docs_good]
-GO
-
-ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_V_Category_docs_passive] FOREIGN KEY([v_docs_passive])
-REFERENCES [dbo].[CSP_V_Category] ([id])
-GO
-ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_V_Category_docs_passive]
-GO
-
-ALTER TABLE [dbo].[CSP_Response]  WITH CHECK ADD  CONSTRAINT [FK_CSP_Response_CSP_V_Category_docs_poor] FOREIGN KEY([v_docs_poor])
-REFERENCES [dbo].[CSP_V_Category] ([id])
-GO
-ALTER TABLE [dbo].[CSP_Response] CHECK CONSTRAINT [FK_CSP_Response_CSP_V_Category_docs_poor]
+ALTER TABLE [dbo].[CSP_V_Analysis] CHECK CONSTRAINT [FK_CSP_V_Analysis_CSP_V_Category]
 GO
 
 
 /********************
 	ADD QUESTIONS
 *********************/
-insert into CSP_Question values(1,'How likely is it that you would recommend ITRS Group to a friend or colleague?')
-insert into CSP_Question values(2,'How satisfied are you with ITRS Geneos?')
-insert into CSP_Question values(3,'How satisfied are you with ITRS OP5 Monitor?')
-insert into CSP_Question values(4,'How satisfied are you with the quality of support you receive from ITRS Group?')
-insert into CSP_Question values(5,'How satisfied are you with the revised online documentation at docs.itrsgroup.com?')
-insert into CSP_Question values(6,'What would you like to see improved?')
-insert into CSP_Question values(7,'How likely are you to buy from ITRS again?')
-insert into CSP_Question values(8,'How well does ITRS products strategy and roadmap meet your business needs?')
-insert into CSP_Question values(9,'How helpful do you find the online documentation at resources.itrsgroup.com?')
-insert into CSP_Question values(10,'How useful is ITRS move to an open architecture product? (Well defined APIs and interfaces to integrate, extend and customise)')
+insert into CSP_Question values('How likely is it that you would recommend ITRS Group to a friend or colleague?')
+insert into CSP_Question values('How satisfied are you with ITRS Geneos?')
+insert into CSP_Question values('How satisfied are you with ITRS OP5 Monitor?')
+insert into CSP_Question values('How satisfied are you with the quality of support you receive from ITRS Group?')
+insert into CSP_Question values('How satisfied are you with the revised online documentation at docs.itrsgroup.com?')
+insert into CSP_Question values('What would you like to see improved?')
+insert into CSP_Question values('How likely are you to buy from ITRS again?')
+insert into CSP_Question values('How well does ITRS products strategy and roadmap meet your business needs?')
+insert into CSP_Question values('How helpful do you find the online documentation at resources.itrsgroup.com?')
+insert into CSP_Question values('How useful is ITRS move to an open architecture product? (Well defined APIs and interfaces to integrate, extend and customise)')
+
+
+/********************
+	ADD AREAS
+*********************/
+insert into CSP_V_Area values('ITRS','Good')
+insert into CSP_V_Area values('ITRS','Passive')
+insert into CSP_V_Area values('ITRS','Poor')
+insert into CSP_V_Area values('Product','Good')
+insert into CSP_V_Area values('Product','Passive')
+insert into CSP_V_Area values('Product','Poor')
+insert into CSP_V_Area values('Support','Good')
+insert into CSP_V_Area values('Support','Passive')
+insert into CSP_V_Area values('Support','Poor')
+insert into CSP_V_Area values('Docs','Good')
+insert into CSP_V_Area values('Docs','Passive')
+insert into CSP_V_Area values('Docs','Poor')
 
 
 /********************
 	ADD CATEGORIES
 *********************/
-insert into CSP_V_Category values(1,'Hard to learn/use')
-insert into CSP_V_Category values(2,'Bugs/crashes')
-insert into CSP_V_Category values(3,'Roadmap/new features')
-insert into CSP_V_Category values(4,'Support')
-insert into CSP_V_Category values(5,'Price')
-insert into CSP_V_Category values(6,'Documentation')
-insert into CSP_V_Category values(7,'Performance')
-insert into CSP_V_Category values(8,'Dashboards')
-insert into CSP_V_Category values(9,'Bug resolution')
-insert into CSP_V_Category values(10,'UI')
-insert into CSP_V_Category values(11,'Difficult to get training')
-insert into CSP_V_Category values(12,'Communication')
-insert into CSP_V_Category values(13,'Trial support')
-insert into CSP_V_Category values(14,'Sales')
-insert into CSP_V_Category values(15,'Usability')
-insert into CSP_V_Category values(16,'Best tool')
-insert into CSP_V_Category values(17,'Flexibility')
-insert into CSP_V_Category values(18,'Reliable')
-insert into CSP_V_Category values(19,'Real time')
-insert into CSP_V_Category values(20,'Automation')
-
-
+insert into CSP_V_Category values('Hard to learn/use')
+insert into CSP_V_Category values('Bugs/crashes')
+insert into CSP_V_Category values('Roadmap/new features')
+insert into CSP_V_Category values('Support')
+insert into CSP_V_Category values('Price')
+insert into CSP_V_Category values('Documentation')
+insert into CSP_V_Category values('Performance')
+insert into CSP_V_Category values('Dashboards')
+insert into CSP_V_Category values('Bug resolution')
+insert into CSP_V_Category values('UI')
+insert into CSP_V_Category values('Difficult to get training')
+insert into CSP_V_Category values('Communication')
+insert into CSP_V_Category values('Trial support')
+insert into CSP_V_Category values('Sales')
+insert into CSP_V_Category values('Usability')
+insert into CSP_V_Category values('Best tool')
+insert into CSP_V_Category values('Flexibility')
+insert into CSP_V_Category values('Reliable')
+insert into CSP_V_Category values('Real time')
+insert into CSP_V_Category values('Automation')
+insert into CSP_V_Category values('General')
+insert into CSP_V_Category values('Outdated/behind')
+insert into CSP_V_Category values('Examples')
+insert into CSP_V_Category values('Licensing')
--- Database/CSP_select.sql
+++ Database/CSP_select.sql
@@ -1,30 +1,9 @@
 
-select * from Organisation
-select * from Users
-select * from CSP_Question
-select * from CSP_V_Category order by category
+select * from CSP_Product$
 select * from CSP_Survey
 select * from CSP_Survey_User
-select * from CSP_Survey_User where survey_id = 1
+select * from CSP_Question
 select * from CSP_Response
-
--- VIEW SURVEY RESULTS
--- One row per question per user
-select
-	su.email, q.question, r.score, r.comment,
-	r.v_itrs_good,		r.v_itrs_passive,	 r.v_itrs_poor,
-	r.v_product_good,	r.v_product_passive, r.v_product_poor,
-	r.v_support_good,	r.v_support_passive, r.v_support_poor,
-	r.v_docs_good,		r.v_docs_passive,	 r.v_docs_poor
-from CSP_Response r
-join CSP_Survey_User su on r.survey_user_id = su.id
-join CSP_Question q on r.question_id = q.id
-join CSP_Survey s on su.survey_id = s.id
-where s.id = 1
-
--- VIEW SURVEYS FOR USER
--- One row per survey responded to
-select su.email, s.name, su.end_date
-from CSP_Survey_User su
-join CSP_Survey s on su.survey_id = s.id
-where su.email = 'louis.louca@db.com'
+select * from CSP_V_Area
+select * from CSP_V_Category
+select * from CSP_V_Analysis
